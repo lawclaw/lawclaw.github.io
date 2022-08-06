9a099df5084497dff7230e4c847854dea4f0d305
Vinh Phat Tu <vphatmail@gmail.com>
2022-08-06 12:26:40 +0100

    restart again

--- pages/quiz/questions/multiple_choice_question.js
+++
@@ -1,9 +0,0 @@
-class MultipleChoiceQuestion {
-    constructor(question, context, answers, correct_answer) {
-        this.question = question;
-        this.context = context;
-        this.answers = answers;
-        this.correct_answer = correct_answer;
-    }
-
-}
