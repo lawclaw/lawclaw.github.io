78f612afeed255e583c6ae9753c41836ebc81cf9
Vinh Tu <vphatmail@gmail.com>
2022-11-16 22:41:23 +0000

    removed all

--- pages/quiz/questions/written_question.js
+++
@@ -1,7 +0,0 @@
-class WrittenQuestion {
-    constructor(question, context, correct_answer) {
-        this.question = question;
-        this.context = context;
-        this.correct_answer = correct_answer;
-    }
-}
